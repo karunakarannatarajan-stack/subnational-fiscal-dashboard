389105b848b7763a5825a4360237ccbde18ed347
User <user@example.com>
2026-07-12 12:38:53 +0800

    feat: add Fiscal Return on Central Contribution line chart at the bottom of trajectory tab

--- data.js
+++ data.js
@@ -669,8 +669,20 @@ const centralInvestmentBreakdown = {
   }
 };
 
+// Estimated State Contribution to Central Taxes (% of GSDP)
+const centralTaxContribution = {
+  MH: [8.5, 8.6, 8.8, 9.0, 9.2, 9.1, 8.9, 8.8, 8.7, 9.0, 9.2, 8.9, 8.8, 8.7, 8.6, 8.5],
+  TN: [6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.2, 7.4, 7.2, 7.1, 7.0, 6.9, 6.8],
+  GJ: [5.8, 5.9, 6.1, 6.3, 6.5, 6.4, 6.3, 6.2, 6.1, 6.3, 6.5, 6.3, 6.2, 6.1, 6.0, 5.9],
+  KA: [6.8, 6.9, 7.1, 7.3, 7.5, 7.4, 7.3, 7.2, 7.1, 7.3, 7.5, 7.3, 7.2, 7.1, 7.0, 6.9],
+  UP: [2.5, 2.6, 2.7, 2.8, 2.9, 2.9, 2.8, 2.7, 2.6, 2.8, 2.9, 2.8, 2.7, 2.6, 2.5, 2.4],
+  AP: [3.2, 3.3, 3.4, 3.5, 3.6, 3.5, 3.4, 3.3, 3.2, 3.4, 3.6, 3.4, 3.3, 3.2, 3.1, 3.0],
+  TS: [null, null, null, null, 4.8, 4.9, 5.0, 5.1, 5.2, 5.1, 5.0, 4.9, 4.8, 4.7, 4.6, 4.5],
+  WB: [3.5, 3.6, 3.7, 3.8, 3.9, 3.8, 3.7, 3.6, 3.5, 3.7, 3.9, 3.7, 3.6, 3.5, 3.4, 3.3]
+};
+
 if (typeof module !== "undefined" && module.exports) {
-  module.exports = { fiscalData, centralInvestmentBreakdown };
+  module.exports = { fiscalData, centralInvestmentBreakdown, centralTaxContribution };
 } else {
   window.fiscalData = fiscalData;
   window.centralInvestmentBreakdown = centralInvestmentBreakdown;
